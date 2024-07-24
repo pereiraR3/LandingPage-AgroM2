0b45edd416e787fa3856b924a5ba31e49149233a
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-24 15:30:24 -0400

    Edit. css

--- AgroM2/src/components/IntermedSection/IntermedSection.jsx
+++ AgroM2/src/components/IntermedSection/IntermedSection.jsx
@@ -8,7 +8,7 @@ const IntermedSection = () => {
   return (
     <div className="py-16 px-8 md:px-16 lg:px-32 bg-white relative">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-        <div>
+        <div className="relative">
           <ul className="list-disc space-y-4 text-3xl md:text-4xl lg:text-6xl font-bold">
             <li className="text-black">Análise</li>
             <li className="text-black">Fornecimento</li>
@@ -17,9 +17,13 @@ const IntermedSection = () => {
           <p className="text-black text-md md:text-lg lg:text-xl mt-4">
             Saiba mais sobre nossos serviços
           </p>
+          <div className="plants-container absolute top-0 right-0 mt-8 md:mt-0">
+            <img src={plant01} alt="plant" className="plant plant-01 mb-4" />
+            <img src={plant02} alt="plant" className="plant plant-02" />
+          </div>
         </div>
         <div className="relative flex justify-center items-center">
-            <img src={farmer} alt="Farmer" className="w-full max-w-md rounded-lg" />
+          <img src={farmer} alt="Farmer" className="w-full max-w-md rounded-lg" />
         </div>
       </div>
     </div>
